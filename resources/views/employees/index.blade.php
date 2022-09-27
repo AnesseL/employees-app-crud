ff53b504e764c5f40bca440b0667263f874315a1
Agnieszka Leśków <agnieszkaleskow@gmail.com>
2022-09-27 13:46:19 +0200

    #4 fourth commit: made controller, route and check is it working

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -4,6 +4,7 @@
     <section class="container">
         <div class="row justify-content-center">
             <h1 class="py-5">Employees List</h1>
+            <h2>{{ $name }}</h2>
         </div>
     </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('employees.index');
-})->name('employees.list');
+Route::get('/', [EmployeesController::class, 'index'])->name('employees.list');
 
 Route::get('/about-us', function () {
     return view('pages.about');
